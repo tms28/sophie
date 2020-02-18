b9022bac3e705ebcb6b84a13064e0216e7b864c4
Thomas Lacheze <thomas.lchz@gmail.com>
2020-02-18 16:56:45 +0100

    generated main route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'main', to: 'pages#main', as: :main
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
